a40767867067431cbf31981e7b57f3382ae17a4d
Phil Jensen <phil.e.jensen@gmail.com>
2016-04-01 12:44:46 -0500

    rspec setup with a failing test

--- spec/controllers/hello_controller_spec.rb
+++ spec/controllers/hello_controller_spec.rb
@@ -1,1 +1,11 @@
- 
+describe HelloController do 
+   context "When testing the HelloController class" do 
+      
+      it "should set the greeting field to 'Hello World!'" do 
+         hc = HelloController.new 
+         message = hc.greeting
+         expect(message).to eq "Hello World!"
+      end
+      
+   end
+end
