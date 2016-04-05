8341313a3476cf75cbf4da39e6b89a8b400a8cda
Phil Jensen <phil.e.jensen@gmail.com>
2016-04-05 08:58:18 -0500

    added initializer that takes a client

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -2,9 +2,16 @@ require 'base_controller'
 
 class HelloController < BaseController
 	
+	def self.build(api_key = ENV['ORCHESTRATE_KEY'])
+		new (Orchestrate::Client.new(:api_key => api_key))
+	end
+
+	def initialize(client)
+		@client = client
+	end
+
 	def index
 		@greeting =  "Hello World!"
 	end
-
 
 end
--- spec/controllers/hello_controller_spec.rb
+++ spec/controllers/hello_controller_spec.rb
@@ -1,7 +1,11 @@
 describe HelloController do 
 
+   before :each do
+   	 @controller = HelloController.new(nil) 
+   end
+
    render_views
-   
+
    context "Route for HelloController class" do
 	  it "routes /hello to the HelloController" do
 	    expect({ :get => "/hello" }).to route_to(:controller => "hello", :action => "index")
@@ -16,7 +20,7 @@ describe HelloController do
 	    get :index
 	    expect(response).to have_http_status(:unauthorized)
 	  end
-	  it "invalid creds XXX" do
+	  it "invalid creds 401" do
 	    request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials('foo', 'bar')
 	    get :index
 	    expect(response).to have_http_status(:unauthorized)
@@ -31,10 +35,16 @@ describe HelloController do
 
    context "When testing the HelloController class" do 
       it "should set the greeting field to 'Hello World!'" do 
-         hc = HelloController.new 
-         hc.index
-         message = hc.instance_variable_get(:@greeting)
+         @controller.index
+         message = @controller.instance_variable_get(:@greeting)
          expect(message).to eq "Hello World!"
       end
    end
+=begin
+   context "Orchestrate client cool stuff"
+   		let(:client) {mock('client')}
+   		it "gets passed to the right place" do
+   			hc = HelloController.new(client)
+   end
+=end
 end
